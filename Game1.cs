bc0675bd704ac5def8f7177d84418089d05b10c2
Maxime Mazet <contact@maximemazet.fr>
2021-01-25 22:52:20 +0100

    Ajout de link et de la résolution NES

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using ZeldaOne.Entities.Friendlies;
+using ZeldaOne.Management;
 
 namespace ZeldaOne
 {
@@ -9,16 +11,23 @@ namespace ZeldaOne
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Link _link;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
+            _link = new Link();
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 512;
+            _graphics.PreferredBackBufferHeight = 478;
+            _graphics.ApplyChanges();
+            
+            _link.Initialize();
 
             base.Initialize();
         }
@@ -26,16 +35,23 @@ namespace ZeldaOne
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            _link.LoadContent(Content);
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            GamePadManager.GamePadState = GamePad.GetState(PlayerIndex.One);
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+
+            _link.Update(gameTime);
+            
+            GamePadManager.OldGamePadState = GamePadManager.GamePadState;
 
             base.Update(gameTime);
         }
@@ -44,6 +60,9 @@ namespace ZeldaOne
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            _spriteBatch.Begin();
+            _link.Draw(_spriteBatch);
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
