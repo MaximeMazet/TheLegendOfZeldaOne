366c54c434b974bff41ec6fc5273cc2f7d7fa571
Maxime Mazet <contact@maximemazet.fr>
2021-01-25 22:51:52 +0100

    Gestion de la manette

---
+++ Management/GamePadManager.cs
@@ -0,0 +1,21 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ZeldaOne.Management
+{
+    public class GamePadManager
+    {
+        public static GamePadState GamePadState;
+        public static GamePadState OldGamePadState;
+
+        public static bool ButtonPressedOnce(Buttons buttons)
+        {
+            return GamePadState.IsButtonDown(buttons) && !OldGamePadState.IsButtonDown(buttons);
+        }
+
+        public static bool ButtonPressedContinue(Buttons buttons)
+        {
+            return GamePadState.IsButtonDown(buttons);
+        }
+    }
+}
